a44c40aaadf2783a55cc65d7885d46313ddfd1d0
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2025-03-06 15:32:03 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bryan Martinez on 6/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
